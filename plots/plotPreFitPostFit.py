966d0fec0cf2a6e1465152f671deffeb86bcedab
Brandon <ballen@mit.edu>
2016-09-01 13:08:09 -0400

    added first pass at systematics (only normalizations) and switched back to shape.

--- plots/plotPreFitPostFit.py
+++ plots/plotPreFitPostFit.py
@@ -541,7 +541,7 @@ def plotPreFitPostFit(region,category,sb=False):
 
   gPad.RedrawAxis()
 
-  folder = "/home/ballen/public_html/cmsplots/monophoton/combinedfit/onebin"
+  folder = "/home/ballen/public_html/cmsplots/monophoton/combinedfit/shape"
   c.SaveAs(folder+"/"+category+"_prefit_postfit_"+region+".pdf")
   c.SaveAs(folder+"/"+category+"_prefit_postfit_"+region+".png")
   # c.SaveAs(folder+"/"+category+"_prefit_postfit_"+region+".C")
--- plots/plot_ratio.py
+++ plots/plot_ratio.py
@@ -174,7 +174,7 @@ def plot_ratio(process,category):
     
     gPad.RedrawAxis()
 
-    folder ="/home/ballen/public_html/cmsplots/monophoton/combinedfit/onebin"
+    folder ="/home/ballen/public_html/cmsplots/monophoton/combinedfit/shape"
 
     c.SaveAs(folder+"/rfactor_"+category+"_"+process+".pdf")
     c.SaveAs(folder+"/rfactor_"+category+"_"+process+".png")
