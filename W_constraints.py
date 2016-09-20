d443324716bd60e0d2b0cfc4f3d6e85a11e1f723
Brandon <ballen@mit.edu>
2016-09-20 12:49:04 -0400

    Attempting to switch off / remove any statistical uncertainty on the transfer factors. Doesn't seem to have any effect. ????

--- W_constraints.py
+++ W_constraints.py
@@ -30,10 +30,16 @@ def cmodel(cid,nam,_f,_fOut, out_ws, diag):
   # Create the transfer factors and save them (not here you can also create systematic variations of these 
   # transfer factors (named with extention _sysname_Up/Down
   WmnScales = targetmc.Clone(); WmnScales.SetName("monomu_weights_%s"%cid)
-  WmnScales.Divide(controlmc);  _fOut.WriteTObject(WmnScales)  # always write out to the directory 
+  WmnScales.Divide(controlmc);  
+  for iBin in range(1, WmnScales.GetNbinsX()+1):
+    WmnScales.SetBinError(iBin, 0.)
+  _fOut.WriteTObject(WmnScales)  # always write out to the directory 
 
   WenScales = targetmc.Clone(); WenScales.SetName("monoel_weights_%s"%cid)
-  WenScales.Divide(controlmc_e);  _fOut.WriteTObject(WenScales)  # always write out to the directory 
+  WenScales.Divide(controlmc_e);  
+  for iBin in range(1, WenScales.GetNbinsX()+1):
+    WenScales.SetBinError(iBin, 0.)
+  _fOut.WriteTObject(WenScales)  # always write out to the directory 
 
   ## Lepton Scale factors
   """
@@ -74,34 +80,9 @@ def cmodel(cid,nam,_f,_fOut, out_ws, diag):
   # these must be created and writted to the same dirctory as the nominal (fDir)
 
   # Statistical uncertainties too!, one per bin 
-  for b in range(targetmc.GetNbinsX()):
-    err = WmnScales.GetBinError(b+1)
-    if not WmnScales.GetBinContent(b+1)>0: continue 
-    relerr = err/WmnScales.GetBinContent(b+1)
-    if relerr<0.001: continue
-    byb_u = WmnScales.Clone(); byb_u.SetName("monomu_weights_%s_%s_stat_error_%s_bin%d_Up"%(cid,cid,"monomuCR",b))
-    byb_u.SetBinContent(b+1,WmnScales.GetBinContent(b+1)+err)
-    byb_d = WmnScales.Clone(); byb_d.SetName("monomu_weights_%s_%s_stat_error_%s_bin%d_Down"%(cid,cid,"monomuCR",b))
-    byb_d.SetBinContent(b+1,WmnScales.GetBinContent(b+1)-err)
-    _fOut.WriteTObject(byb_u)
-    _fOut.WriteTObject(byb_d)
-    print "Adding an error -- ", byb_u.GetName(),err
-    CRs[0].add_nuisance_shape("%s_stat_error_%s_bin%d"%(cid,"monomuCR",b),_fOut)
 
-  # Statistical uncertainties too!, one per bin 
-  for b in range(targetmc.GetNbinsX()):
-    err_e = WenScales.GetBinError(b+1)
-    if not WenScales.GetBinContent(b+1)>0: continue 
-    relerr_e = err_e/WenScales.GetBinContent(b+1)
-    if relerr_e<0.001: continue
-    byb_u_e = WenScales.Clone(); byb_u_e.SetName("monoel_weights_%s_%s_stat_error_%s_bin%d_Up"%(cid,cid,"monoelCR",b))
-    byb_u_e.SetBinContent(b+1,WenScales.GetBinContent(b+1)+err_e)
-    byb_d_e = WenScales.Clone(); byb_d_e.SetName("monoel_weights_%s_%s_stat_error_%s_bin%d_Down"%(cid,cid,"monoelCR",b))
-    byb_d_e.SetBinContent(b+1,WenScales.GetBinContent(b+1)-err_e)
-    _fOut.WriteTObject(byb_u_e)
-    _fOut.WriteTObject(byb_d_e)
-    print "Adding an error -- ", byb_u_e.GetName(),err_e
-    CRs[1].add_nuisance_shape("%s_stat_error_%s_bin%d"%(cid,"monoelCR",b),_fOut)
+  # CRs[0].addUncorrStatSysts(targetmc, WmnScales, "monomu", "monomu", cid, _fOut)
+  # CRs[1].addUncorrStatSysts(targetmc, WenScales, "monoel", "monoel", cid, _fOut)
 
   #######################################################################################################
 
--- Z_constraints.py
+++ Z_constraints.py
@@ -31,10 +31,16 @@ def cmodel(cid,nam,_f,_fOut, out_ws, diag):
   # Create the transfer factors and save them (not here you can also create systematic variations of these 
   # transfer factors (named with extention _sysname_Up/Down
   ZmmScales = target.Clone(); ZmmScales.SetName("dimu_weights_%s" %cid)
-  ZmmScales.Divide(controlmc);  _fOut.WriteTObject(ZmmScales)  # always write out to the directory 
+  ZmmScales.Divide(controlmc);  
+  for iBin in range(1, ZmmScales.GetNbinsX()+1):
+    ZmmScales.SetBinError(iBin, 0.)
+  _fOut.WriteTObject(ZmmScales)  # always write out to the directory 
 
   ZeeScales = target.Clone(); ZeeScales.SetName("diel_weights_%s" %cid)
-  ZeeScales.Divide(controlmc_e);  _fOut.WriteTObject(ZeeScales)  # always write out to the directory 
+  ZeeScales.Divide(controlmc_e);  
+  for iBin in range(1, ZeeScales.GetNbinsX()+1):
+    ZeeScales.SetBinError(iBin, 0.)
+  _fOut.WriteTObject(ZeeScales)  # always write out to the directory 
 
   ## Lepton Scale factors
 
@@ -75,39 +81,8 @@ def cmodel(cid,nam,_f,_fOut, out_ws, diag):
   # note, the code will LOOK for something called NOMINAL_name_Up and NOMINAL_name_Down, where NOMINAL=WScales.GetName()
   # these must be created and writted to the same dirctory as the nominal (fDir)
 
-  for b in range(target.GetNbinsX()):
-    err = ZmmScales.GetBinError(b+1)
-    if not ZmmScales.GetBinContent(b+1)>0: continue 
-    relerr = err/ZmmScales.GetBinContent(b+1)
-    if relerr<0.01: continue
-    byb_u = ZmmScales.Clone(); byb_u.SetName("dimu_weights_%s_%s_stat_error_%s_bin%d_Up"%(cid,cid,"dimuCR",b))
-    byb_u.SetBinContent(b+1,ZmmScales.GetBinContent(b+1)+err)
-    byb_d = ZmmScales.Clone(); byb_d.SetName("dimu_weights_%s_%s_stat_error_%s_bin%d_Down"%(cid,cid,"dimuCR",b))
-    if (ZmmScales.GetBinContent(b+1)-err > 0):
-      byb_d.SetBinContent(b+1,ZmmScales.GetBinContent(b+1)-err)
-    else:
-      byb_d.SetBinContent(b+1,1)
-    _fOut.WriteTObject(byb_u)
-    _fOut.WriteTObject(byb_d)
-    print "Adding an error -- ", byb_u.GetName(),err
-    CRs[0].add_nuisance_shape("%s_stat_error_%s_bin%d"%(cid,"dimuCR",b),_fOut)
-
-  for b in range(target.GetNbinsX()):
-    err = ZeeScales.GetBinError(b+1)
-    if not ZeeScales.GetBinContent(b+1)>0: continue 
-    relerr = err/ZeeScales.GetBinContent(b+1)
-    if relerr<0.01: continue
-    byb_u = ZeeScales.Clone(); byb_u.SetName("diel_weights_%s_%s_stat_error_%s_bin%d_Up"%(cid,cid,"dielCR",b))
-    byb_u.SetBinContent(b+1,ZeeScales.GetBinContent(b+1)+err)
-    byb_d = ZeeScales.Clone(); byb_d.SetName("diel_weights_%s_%s_stat_error_%s_bin%d_Down"%(cid,cid,"dielCR",b))
-    if (ZeeScales.GetBinContent(b+1)-err > 0):
-      byb_d.SetBinContent(b+1,ZeeScales.GetBinContent(b+1)-err)
-    else:
-      byb_d.SetBinContent(b+1,1)
-    _fOut.WriteTObject(byb_u)
-    _fOut.WriteTObject(byb_d)
-    print "Adding an error -- ", byb_u.GetName(),err
-    CRs[1].add_nuisance_shape("%s_stat_error_%s_bin%d"%(cid,"dielCR",b),_fOut)
+  # CRs[0].addUncorrStatSysts(target, ZmmScales, "dimu", "dimu", cid, _fOut)
+  # CRs[1].addUncorrStatSysts(target, ZeeScales, "diel", "diel", cid, _fOut)
 
   #######################################################################################################
 
